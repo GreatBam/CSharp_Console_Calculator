9f5c53eab40da22a55bfb996f33114f3f1694e76
Jonathan <jonathan.gabioud@hotmail.com>
2023-09-22 08:38:47 +0200

    Code main program

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -6,7 +6,29 @@ namespace calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int result = 0;
+            int number1 = Tools.AskUserForNumber("Enter the first number : ");
+            int userOperator = Tools.AskUserForOperation();
+            int number2 = Tools.AskUserForNumber("Enter the second number : ");
+            switch (userOperator)
+            {
+                case 1:
+                    result = number1 + number2;
+                    break;
+                case 2:
+                    result = number1 - number2;
+                    break;
+                case 3:
+                    result = number1 * number2;
+                    break;
+                case 4:
+                    result = number1 / number2;
+                    break;
+                default:
+                    Console.WriteLine("Error : Invalid operation");
+                    break;
+            }
+            Console.WriteLine("Your result : " + result);
         }
     }
 }
