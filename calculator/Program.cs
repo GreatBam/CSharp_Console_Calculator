aa5dbaf23d191f7a551e2384c41cd12638c42438
Jonathan <jonathan.gabioud@hotmail.com>
2023-09-22 08:25:01 +0200

    Add files

---
+++ calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
