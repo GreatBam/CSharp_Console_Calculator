860509b94410e855bf3335f6dfc03e23d9a92107
Jonathan <jonathan.gabioud@hotmail.com>
2023-09-22 08:40:01 +0200

    Add while loop in main

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -7,28 +7,32 @@ namespace calculator
         static void Main(string[] args)
         {
             int result = 0;
-            int number1 = Tools.AskUserForNumber("Enter the first number : ");
-            int userOperator = Tools.AskUserForOperation();
-            int number2 = Tools.AskUserForNumber("Enter the second number : ");
-            switch (userOperator)
+            bool again = true;
+            do
             {
-                case 1:
-                    result = number1 + number2;
-                    break;
-                case 2:
-                    result = number1 - number2;
-                    break;
-                case 3:
-                    result = number1 * number2;
-                    break;
-                case 4:
-                    result = number1 / number2;
-                    break;
-                default:
-                    Console.WriteLine("Error : Invalid operation");
-                    break;
-            }
-            Console.WriteLine("Your result : " + result);
+                int number1 = Tools.AskUserForNumber("Enter the first number : ");
+                int userOperator = Tools.AskUserForOperation();
+                int number2 = Tools.AskUserForNumber("Enter the second number : ");
+                switch (userOperator)
+                {
+                    case 1:
+                        result = number1 + number2;
+                        break;
+                    case 2:
+                        result = number1 - number2;
+                        break;
+                    case 3:
+                        result = number1 * number2;
+                        break;
+                    case 4:
+                        result = number1 / number2;
+                        break;
+                    default:
+                        Console.WriteLine("Error : Invalid operation");
+                        break;
+                }
+                Console.WriteLine("Your result : " + result);
+            }while (again);
         }
     }
 }
