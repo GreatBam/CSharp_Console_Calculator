47e614c983c89512941d1ccfb6883dd73b9ab84d
Jonathan <jonathan.gabioud@hotmail.com>
2023-09-22 10:15:31 +0200

    Implement ,ethod for result in main

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -6,33 +6,13 @@ namespace calculator
     {
         static void Main(string[] args)
         {
-            int result = 0;
             bool again = true;
             do
             {
                 int number1 = Tools.AskUserForNumber("Enter the first number : ");
                 int userOperator = Tools.AskUserForOperation();
                 int number2 = Tools.AskUserForNumber("Enter the second number : ");
-                switch (userOperator)
-                {
-                    case 1:
-                        result = number1 + number2;
-                        break;
-                    case 2:
-                        result = number1 - number2;
-                        break;
-                    case 3:
-                        result = number1 * number2;
-                        break;
-                    case 4:
-                        result = number1 / number2;
-                        break;
-                    default:
-                        Console.WriteLine("Error : Invalid operation");
-                        break;
-                }
-                Console.WriteLine("Your result : " + result);
-                Console.WriteLine();
+                Tools.DisplayResult(userOperator, number1, number2);
                 again = Tools.AskUserForContinue();
             }while (again);
         }
