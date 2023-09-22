dbcb35ce9fa44dc6a98b5e07f99b50ad9bb207d3
Jonathan <jonathan.gabioud@hotmail.com>
2023-09-22 08:31:14 +0200

    Made method to ask for number

--- calculator/Tools.cs
+++ calculator/Tools.cs
@@ -4,6 +4,21 @@ namespace calculator
 {
     internal class Tools
     {
-        
+        public static int AskUserForNumber(string message)
+        {
+            int number = 0;
+            Console.Write(message);
+            string input = Console.ReadLine();
+            try
+            {
+                number = int.Parse(input);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error : " + e);
+                throw;
+            }
+            return number;
+        }
     }
 }
